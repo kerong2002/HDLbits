e213cb280c6e8005fcd3c2d26a73555a5b71568f
kerong <70834651+kerong2002@users.noreply.github.com>
2022-06-04 20:23:30 +0800

    Add files via upload

---
+++ Browse problem set/Getting Started/Output zero.v
@@ -0,0 +1,9 @@
+//2022-02-22 陳科融
+//Output zero
+module top_module(
+    output zero
+);// Module body starts after semicolon
+
+assign zero=0;
+
+endmodule
---
+++ Browse problem set/Verilog Language/Basics/1.Simple wire.v
@@ -0,0 +1,7 @@
+//2022-02-22 陳科融
+//Simple wire
+module top_module( input in, output out );
+
+assign  out=in;
+
+endmodule
