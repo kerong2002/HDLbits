f251e4a436dce9c343b893ae3beb9822db34c5e0
kerong <70834651+kerong2002@users.noreply.github.com>
2022-09-22 12:41:01 +0800

    Delete Browse problem set/Getting Started directory

--- Browse problem set/Getting Started/Getting start.v
+++
@@ -1,8 +0,0 @@
-//2022-02-22 陳科融
-//Getting start
-module top_module( output one );
-
-// Insert your code here
-    assign one = 1'b1;
-
-endmodule
--- Browse problem set/Getting Started/Output zero.v
+++
@@ -1,9 +0,0 @@
-//2022-02-22 陳科融
-//Output zero
-module top_module(
-    output zero
-);// Module body starts after semicolon
-
-assign zero=0;
-
-endmodule
