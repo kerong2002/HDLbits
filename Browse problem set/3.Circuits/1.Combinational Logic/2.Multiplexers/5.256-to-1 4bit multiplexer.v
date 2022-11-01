2e01a89889f75f68942346c5b5610b1143eb6ac2
kerong <70834651+kerong2002@users.noreply.github.com>
2022-11-01 16:13:06 +0800

    Update 5.256-to-1 4bit multiplexer.v

--- Browse problem set/3.Circuits/1.Combinational Logic/2.Multiplexers/5.256-to-1 4bit multiplexer.v
+++ Browse problem set/3.Circuits/1.Combinational Logic/2.Multiplexers/5.256-to-1 4bit multiplexer.v
@@ -5,7 +5,7 @@ module top_module(
     input [7:0] sel,
     output [3:0] out );
 
-assign out = {in[sel*4+3], in[sel*4+2], in[sel*4+1], in[sel*4+0]};
+//assign out = {in[sel*4+3], in[sel*4+2], in[sel*4+1], in[sel*4+0]};
 assign out = in[sel*4+:4];
 
 endmodule
